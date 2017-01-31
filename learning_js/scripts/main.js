392e995da9791d8a6f983611d64809adaf11c611
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-MBP.home>
2017-01-31 02:41:52 -0500

    initial files

---
+++ learning_js/scripts/main.js
@@ -0,0 +1,11 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent = "Hello World!";
+
+
+var icecream = "chocolate";
+
+if (icecream === "chocolate"){
+	alert("Yay the icecream is chocolate");
+}	else {
+	alert("Well I'm not even gonna consider this");
+}
