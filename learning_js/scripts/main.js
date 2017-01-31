def31d5c4653c757729fb6f6e891f1c9b34f8981
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-MBP.home>
2017-01-31 04:09:15 -0500

    initial files

--- learning_js/scripts/main.js
+++ learning_js/scripts/main.js
@@ -9,3 +9,25 @@ if (icecream === "chocolate"){
 }	else {
 	alert("Well I'm not even gonna consider this");
 }
+
+function multiply(num1, num2){
+	var result = num1 * num2;
+	return result;
+	console.log(result); 
+}
+
+multiply(4, 7)
+
+
+
+
+var myimage = document.querySelector('img');
+
+myimage.onclick = function(){
+	var mysrc = myimage.getAttribute('src');
+	if(mysrc === 'images/cow1.jpg'){
+		myimage.setAttribute('src', 'images/cow2.jpg');
+	}	else {
+		myimage.setAttribute('src', 'images/cow1.jpg');
+	}
+}
